1317313c7f4e28b1b42c82e091c14dcda025cc50
Hyunsoo, Chung <admin@hyonsu.com>
2021-10-17 03:11:40 +0900

    fix json parsing

--- utils.js
+++ utils.js
@@ -1,14 +1,20 @@
+const JSON5 = require('json5');
+
 String.prototype.replaceAll = function(org, dest) {
     return this.split(org).join(dest);
 }
 
 module.exports.ADOFAIParser = level => {
-    return JSON.parse(String(level).trim()
-        .replaceAll(', ,', ',')
-        .replaceAll('}\n', '},\n')
-        .replaceAll('},\n\t]', '}\n\t]')
-        .replaceAll(', },', ' },')
-        .replaceAll(', }', ' }')
-        .replaceAll('\n', '')
-        .replaceAll('}\n', '},\n'));
+    try {
+        return JSON5.parse(String(level).trim());
+    } catch (e) {
+        return JSON5.parse(String(level).trim()
+            .replaceAll(', ,', ',')
+            .replaceAll('}\n', '},\n')
+            .replaceAll('},\n\t]', '}\n\t]')
+            .replaceAll(', },', ' },')
+            .replaceAll(', }', ' }')
+            .replaceAll('\n', '')
+            .replaceAll('}\n', '},\n'));
+    }
 }
